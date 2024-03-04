70d1fc35bfac3124505b544133b165cdf4a6fdd4
Jack <jack.p.sarle.acc@gmail.com>
2024-03-03 19:10:54 -0800

    removed nohup from command

--- noip-duc/noip-duc_setup.sh
+++ noip-duc/noip-duc_setup.sh
@@ -6,4 +6,4 @@ wget https://dmej8g5cpdyqd.cloudfront.net/downloads/noip-duc_3.0.0.tar.gz
 tar xf noip-duc_3.0.0.tar.gz
 cd noip-duc_3.0.0/binaries
 apt-get install ./noip-duc_3.0.0_amd64.deb
-echo "nohup noip-duc -g vbnetwork.ddns.me -u jack.p.sarle.acc@gmail.com -p i6qCzyKEdzPkKWY &" >> /root/.bashrc
+echo "noip-duc -g vbnetwork.ddns.me -u jack.p.sarle.acc@gmail.com -p i6qCzyKEdzPkKWY &" >> /root/.bashrc
